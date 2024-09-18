433e5c3e42e3894a018f7f9d4835aff0265c0a53
nsdezcan <nsdezcan@gmail.com>
2024-09-18 22:07:56 +0300

    versiyon3 yazdırdık

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,10 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("versiyon1");
+        System.out.println("versiyon2");
+        System.out.println("versiyon3");
+
+
+
     }
 }
